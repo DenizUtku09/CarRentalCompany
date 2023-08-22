9f2c8aa9304caa7577d14a4579ce4d8177095833
Deniz Utku <93544206+DenizUtku09@users.noreply.github.com>
2023-08-22 09:05:05 +0300

    first

---
+++ src/main/java/ashina/carrental/carrentalcompany/CarRentalCompanyApplication.java
@@ -0,0 +1,13 @@
+package ashina.carrental.carrentalcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ashina/carrental/carrentalcompany/CarRentalCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package ashina.carrental.carrentalcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
